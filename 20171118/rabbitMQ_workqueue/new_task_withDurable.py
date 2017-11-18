19dc2b5ae37501d70c92ee29e79725c5c4d70fce
endercaster <mstext0@hotmail.com>
2017-11-18 13:55:00 +0000

    rabbitmq work queue learning

---
+++ 20171118/rabbitMQ_workqueue/new_task_withDurable.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import pika
+import sys
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+message = ' '.join(sys.argv[1:]) or "Hello World!"
+channel.basic_publish(exchange='',routing_key='hello',body=message,properties=pika.BasicProperties(delivery_mode = 2,))
+print " [x] Sent %r" % (message,)
+connection.close()
