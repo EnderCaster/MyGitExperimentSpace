26b7175d35755a1ad468620cdb958a52336e97c3
EnderCaster <mstext0@hotmail.com>
2017-12-12 10:51:46 +0800

    mysql select

--- 20171205/mysql/mysql_course.sql
+++ 20171205/mysql/mysql_course.sql
@@ -807,8 +807,9 @@ DROP TABLE IF EXISTS `department`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `department` (
-  `dpt_name` char(20) DEFAULT NULL,
-  `dpt_phone` int(12) DEFAULT NULL
+  `dpt_name` char(20) NOT NULL,
+  `people_num` int(10) DEFAULT '10',
+  PRIMARY KEY (`dpt_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -818,6 +819,7 @@ CREATE TABLE `department` (
 
 LOCK TABLES `department` WRITE;
 /*!40000 ALTER TABLE `department` DISABLE KEYS */;
+INSERT INTO `department` VALUES ('dpt1',11),('dpt2',12),('dpt3',10),('dpt4',15);
 /*!40000 ALTER TABLE `department` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -829,9 +831,16 @@ DROP TABLE IF EXISTS `employee`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `employee` (
-  `id` int(10) DEFAULT NULL,
+  `id` int(10) NOT NULL,
   `name` char(20) DEFAULT NULL,
-  `phone` int(12) DEFAULT NULL
+  `age` int(10) DEFAULT NULL,
+  `salary` int(10) NOT NULL,
+  `phone` int(12) NOT NULL,
+  `in_dpt` char(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `phone` (`phone`),
+  KEY `emp_fk` (`in_dpt`),
+  CONSTRAINT `emp_fk` FOREIGN KEY (`in_dpt`) REFERENCES `department` (`dpt_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -841,8 +850,35 @@ CREATE TABLE `employee` (
 
 LOCK TABLES `employee` WRITE;
 /*!40000 ALTER TABLE `employee` DISABLE KEYS */;
-INSERT INTO `employee` VALUES (1,'Tom',110110110),(1,'Jack',110110110),(3,'Rose',NULL);
+INSERT INTO `employee` VALUES (1,'Tom',26,2500,119119,'dpt4'),(2,'Jack',24,2500,120120,'dpt2'),(3,'Rose',22,2800,114114,'dpt3'),(4,'Jim',35,3000,100861,'dpt1'),(5,'Mary',21,3000,100101,'dpt2'),(6,'Alex',26,3000,123456,'dpt1'),(7,'Ken',27,3500,654321,'dpt1'),(8,'Rick',24,3500,987654,'dpt3'),(9,'Joe',31,3600,110129,'dpt2'),(10,'Mike',23,3400,110110,'dpt4'),(11,'Jobs',NULL,3600,19283,'dpt2'),(12,'Tony',NULL,3400,102938,'dpt3');
 /*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `project`
+--
+
+DROP TABLE IF EXISTS `project`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `project` (
+  `proj_num` int(10) NOT NULL,
+  `proj_name` char(20) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date DEFAULT '2015-04-01',
+  `of_dpt` char(20) DEFAULT NULL,
+  PRIMARY KEY (`proj_num`,`proj_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `project`
+--
+
+LOCK TABLES `project` WRITE;
+/*!40000 ALTER TABLE `project` DISABLE KEYS */;
+INSERT INTO `project` VALUES (1,'proj_a','2015-01-15','2015-01-31','dpt2'),(2,'proj_b','2015-01-15','2015-02-15','dpt1'),(3,'proj_c','2015-02-01','2015-03-01','dpt4'),(4,'proj_d','2015-02-15','2015-04-01','dpt3'),(5,'proj_e','2015-02-25','2015-03-01','dpt4'),(6,'proj_f','2015-02-26','2015-03-01','dpt2');
+/*!40000 ALTER TABLE `project` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -854,4 +890,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-12-06 19:50:17
+-- Dump completed on 2017-12-12 10:51:20
