0a223ac25b1c08a1301eb67ce6d5825c880dc219
EnderCaster <mstext0@hotmail.com>
2018-05-12 08:30:28 +0000

    python 3 brief tutorial course 7

---
+++ python_path/python3_brief_tutorial/7_palindrome.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+#-*- utf-8 -*-
+s = input("input:")
+z = s[::-1]
+if z == s:
+	print("yes")
+else:
+	print("no")
---
+++ python_path/python3_brief_tutorial/7_word_counter.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+#-*- utf-8 -*-
+s = input("words:")
+print("count = {}".format(len(s.split())))
