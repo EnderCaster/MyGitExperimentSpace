7d1e37f08e6d01463c227ec67f0ae60c4fbded2a
EnderCaster <mstext0@hotmail.com>
2018-06-20 13:57:57 +0000

    python3 brief tutorial course 18

---
+++ python_path/python3_brief_tutorial/18_project_structure/factorial/build/lib/myfact/__init__.py
@@ -0,0 +1,3 @@
+# -*- utf-8 -*-
+from .fact import factorial
+__all__ = ["factorial", ]
---
+++ python_path/python3_brief_tutorial/18_project_structure/factorial/setup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- utf-8 -*-
+""" factorial project"""
+from setuptools import find_packages
+from setuptools import setup
+setup(name='factorial', version='0.1', description="Factorial module.", long_description="A test module for shiyanlou course", platforms=[
+      "Linux"], author="EnderCaster", author_email="mstext0@hotmail.com", url="http://wordpress.endercaster.com", license="MIT", packages=find_packages())
