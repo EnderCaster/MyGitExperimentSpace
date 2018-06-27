cdf8ad179b7fe63cf1df7af7f7ccf051e041eccd
EnderCaster <mstext0@hotmail.com>
2018-06-27 13:49:48 +0000

    flask micro framwork receive request  data

---
+++ python_path/flask_microframework/5_receive_request_data/app.py
@@ -0,0 +1,39 @@
+import flask
+from flask import request
+app = flask.Flask(__name__)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    err = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'], request.form['password']):
+            return login_user(requert.form['username'])
+        else:
+            error = 'Invalid user/password'
+        # the code under the line is a practice of cookie
+        cookieResp = flask.make_response(flask.render_template('login.html'))
+        cookieResp.set_cookie('username', 'username_hashed')
+        return cookieResp
+    if request.method == 'GET':
+        cookie = request.cookie.get('username')
+        if is_login(cookie):
+            return login_user()
+    return flask.render_template('login.html', error=error)
+
+# we can also use request.args.get('username','guest') to get the params in get
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['the_file_name']
+        f.save('/tmp/web_saved_file')
+
+
+def is_login(cookie):
+    pass
+
+
+def login_user(username=''):
+    pass
