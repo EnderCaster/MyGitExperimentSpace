c1e82d818a28f567cc4150b861a3ee0232c58815
EnderCaster <mstext0@hotmail.com>
2018-08-02 20:52:47 +0800

    MTV Create and delete record

--- python_path/Django Basic/2_project/mysite/lib/urls.py
+++ python_path/Django Basic/2_project/mysite/lib/urls.py
@@ -2,7 +2,11 @@
 # -*- utf8 -*-
 from django.urls import path
 from . import views
+
+app_name='lib'
 urlpatterns=[
     path('',views.index,name='index'),
-    path('detail/',views.detail,name='detail')
+    path('detail/',views.detail,name='detail'),
+    path('addBook/',views.addBook,name='addBook'),
+    path('delBook/<int:book_id>',views.deleteBook,name='delBook')
 ]
--- python_path/Django Basic/2_project/mysite/lib/views.py
+++ python_path/Django Basic/2_project/mysite/lib/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import Book
 # Create your views here.
 def index(request):
@@ -9,4 +11,19 @@ def detail(request):
     book_list=Book.objects.order_by('-pub_date')[:5]
     context={'book_list':book_list}
     return render(request,'lib/detail.html',context=context)
+
+def addBook(request):
+    if request.method=='POST':
+        temp_name=request.POST['name']
+        temp_author=request.POST['author']
+        temp_pub_house=request.POST['pub_house']
+
+    from django.utils import timezone
+    temp_book=Book(name=temp_name,author=temp_author,pub_house=temp_pub_house,pub_date=timezone.now())
+    temp_book.save()
+
+    return HttpResponseRedirect(reverse('lib:detail'))
+def deleteBook(request,book_id):
+    Book.objects.filter(id=book_id).delete()
+    return HttpResponseRedirect(reverse('lib:detail'))
     
